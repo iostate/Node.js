e3845e0f6df4c9e0fe68d188b7ef1955d0ccfff5
iostate <qmtruong92@gmail.com>
2022-06-20 20:13:44 -0700

    Error Handling Middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,45 @@
-const errorHandler = (err, req, res, next) => {
-  // Log to console for dev
-  console.log(err.stack.red);
+const ErrorResponse = require('../utils/errorResponse');
 
-  res.status(err.statusCode || 500).json({
+/**
+ * Handle errors. Construct a response to send back to the client
+ * if an error is encountered.
+ * @param {} err
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next Express.js error-handling callback
+ */
+const errorHandler = (err, req, res, next) => {
+  // Initialize an error object for our custom ErrorResponse
+  let error = { ...err };
+  error.message = err.message;
+  // Log to console for dev
+  // Prints out fields that we can USE to perform even better error validation!
+  console.log(err.name);
+
+  // Mongoose bad ObjectId - Handle error if bootcamp not found by id
+  if (err.name === 'CastError') {
+    const message = `Bootcamp not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // POST Error Handling - Duplicate name value entered.
+  // There cannot be 2 bootcamps with the same name in the database.
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered. \nValue = ${JSON.stringify(err.keyValue)}`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Our new nicely formatted error
+  // console.log(error);
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || 'Server Error',
+    error: error.message || 'Server Error',
   });
 };
 
