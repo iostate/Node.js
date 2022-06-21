0345bc7e8baf649c2fcfaafb2052c565595dea94
iostate <qmtruong92@gmail.com>
2022-06-20 20:14:28 -0700

    Add ErrorResponse(message, statusCode)

--- utils/errorResponse.js
+++ utils/errorResponse.js
@@ -1,3 +1,7 @@
+/**
+ * Creates an ErrorResponse object that consists of an
+ * HTTP Status code and a message.
+ */
 class ErrorResponse extends Error {
   constructor(message, statusCode) {
     super(message);
