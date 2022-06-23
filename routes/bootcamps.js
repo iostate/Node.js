1b6644f31f5509875e5ff19033d8681e798b3478
iostate <qmtruong92@gmail.com>
2022-06-22 17:19:21 -0700

    Register route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,11 +7,19 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius,
 } = require('../controllers/bootcamps');
+
+// Register the getBootcampsInRadius controller
+// Imagine the route ends with /api/v1/bootcamps then begins the /radius/:zipcode/:distance
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/').get(getBootcamps).post(createBootcamp);
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
+
+// BEGIN REFACTOR -----
+// The two lines of code above replaced all this
 
 // router.get('/:id', (req, res) => {
 //   res.status(200).json({ success: true, msg: `Show the bootcamp id ${req.params.id}` });
@@ -30,5 +38,5 @@ router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 // });
 
 // router.listen(3000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
+// END REFACTOR -----
 module.exports = router;
