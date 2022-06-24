f9857a683221f6030bc5db6265ee5495438cbaa9
iostate <qmtruong92@gmail.com>
2022-06-24 12:57:26 -0700

    Add bootcamps/:bootcampId/courses * /courses URL

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = express.Router();
 
 const {
   getBootcamps,
@@ -10,33 +9,15 @@ const {
   getBootcampsInRadius,
 } = require('../controllers/bootcamps');
 
-// Register the getBootcampsInRadius controller
-// Imagine the route ends with /api/v1/bootcamps then begins the /radius/:zipcode/:distance
+const courseRouter = require('../routes/courses');
+
+const router = express.Router();
+
+// Re-route into other resource routers.
+// Instead of bringing in getCourses, we'll re-route
+router.use('/:bootcampId/courses', courseRouter);
+
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-
 router.route('/').get(getBootcamps).post(createBootcamp);
-
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
-
-// BEGIN REFACTOR -----
-// The two lines of code above replaced all this
-
-// router.get('/:id', (req, res) => {
-//   res.status(200).json({ success: true, msg: `Show the bootcamp id ${req.params.id}` });
-// });
-
-// router.post('/:id', (req, res) => {
-//   res.status(200).json({ success: true, msg: `Create new bootcamp id ${req.params.id}` });
-// });
-
-// router.put('/:id', (req, res) => {
-//   res.status(200).json({ success: true, msg: `Update bootcamp id ${req.params.id} ` });
-// });
-
-// router.delete('/:id', (req, res) => {
-//   res.status(200).json({ success: true, msg: `Deleted bootcamp id ${req.params.id}` });
-// });
-
-// router.listen(3000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-// END REFACTOR -----
 module.exports = router;
