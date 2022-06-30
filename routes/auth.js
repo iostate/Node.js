04c2bbe29e3b638e8300b13862e597b92cee66e0
iostate <qmtruong92@gmail.com>
2022-06-30 12:49:10 -0700

    Add protection & authentication to auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const { register, getUsers, login } = require('../controllers/auth');
+const { register, getUsers, login, updateUser } = require('../controllers/auth');
 const router = express.Router();
 const User = require('../models/User');
 const advancedResults = require('../middleware/advancedResults');
-const { protect, getMe } = require('../middleware/auth');
+const { protect, getMe, authorize } = require('../middleware/auth');
 
 router.post('/register', register);
 router.route('/login').post(protect, login);
 router.route('/me').post(protect, getMe);
-router.route('/users').get(advancedResults(User), getUsers);
-
+router
+  .route('/users')
+  .get(protect, advancedResults(User), getUsers)
+  .put(protect, authorize('publisher', 'admin'), updateUser)
+  .post(protect, authorize('publisher', 'admin'), register);
 module.exports = router;
