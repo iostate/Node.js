b801b1ec875593ab15a10891b69ef97a898ea37c
iostate <qmtruong92@gmail.com>
2022-06-30 12:46:55 -0700

    Add logic for unique Bootcamp names

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -111,6 +111,17 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
  * @access  Public
  */
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to the request body
+  req.body.user = req.user.id;
+
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If user not an admin, allow only one bootcamp to be added
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`, 400));
+  }
+
+  // Since there was no bootcamp found, we will create the Bootcamp
   const bootcamp = await Bootcamp.create(req.body);
 
   res.status(201).json({
