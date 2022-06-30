4a24c604b7d9ee4a0b6105ab04379e230f3c6b77
iostate <qmtruong92@gmail.com>
2022-06-30 12:48:34 -0700

    Add user to BootcampSchema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -99,6 +99,12 @@ const BootcampSchema = new mongoose.Schema(
       type: Date,
       default: Date.now,
     },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
   },
   {
     toJSON: { virtuals: true },
