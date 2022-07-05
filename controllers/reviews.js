bd5299db567290873d6d82a61c9361b2504b7c33
iostate <qmtruong92@gmail.com>
2022-07-05 16:48:15 -0700

    Get a single review by id, Add review for bootcamp

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -24,3 +24,48 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
     res.status(200).json(res.advancedResults);
   }
 });
+
+/**
+ *
+ * @desc    Get single review
+ * @route   GET /api/v1/reviews/:id
+ * @access  Public
+ */
+exports.getReview = asyncHandler(async (req, res, next) => {
+  //  Check request for an id
+
+  const review = await Review.findById({ bootcamp: req.params.id }).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!review) {
+    return next(new ErrorResponse(`Review of id ${req.params.id} not found`, 404));
+  }
+
+  return res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+/**
+ *
+ * @desc      Add a review for a bootcamp
+ * @route     POST /api/v1/bootcamps/:bootcampId/reviews
+ * @access    Public/User
+ */
+exports.addReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`, 404));
+  }
+
+  const review = await Review.create(req.body);
+
+  res.status(201).json({ success: true, data: review });
+});
