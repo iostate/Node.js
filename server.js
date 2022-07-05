f2dc0e1bfdbca59d350c7da17b27d14fd953827b
iostate <qmtruong92@gmail.com>
2022-07-05 15:23:00 -0700

    Mount reviews routes to server

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
 const users = require('./routes/users');
+const reviews = require('./routes/reviews');
 
 const app = express();
 
@@ -54,7 +55,7 @@ app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
 // Yet to implement
 app.use('/api/v1/users', users);
-// app.use('/api/v1/reviews', reviews);
+app.use('/api/v1/reviews', reviews);
 
 app.use(errorHandler);
 
