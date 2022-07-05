540019a27027a4d63590b8d770c64a2cb69d62b0
iostate <qmtruong92@gmail.com>
2022-07-05 16:48:49 -0700

    Fix JSON Response for get single course

--- controllers/courses.js
+++ controllers/courses.js
@@ -35,7 +35,7 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 
   const course = await Course.create(req.body);
 
-  res.status(200).json({ success: true, data: { course } });
+  res.status(200).json({ success: true, data: course });
 });
 
 /**
