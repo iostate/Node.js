ad8153418942083e4bd5c25f030e76ce645f936f
iostate <qmtruong92@gmail.com>
2022-07-06 15:26:41 -0700

    Remove console.log()

--- models/Course.js
+++ models/Course.js
@@ -45,7 +45,7 @@ const CourseSchema = new mongoose.Schema({
 });
 
 CourseSchema.statics.getAverageCost = async function (bootcampId) {
-  console.log('Calculating avg cost....'.blue);
+  // console.log('Calculating avg cost....'.blue);
 
   //  type !mbda for shortcut Mongoose Snippets
   const obj = await this.aggregate(
