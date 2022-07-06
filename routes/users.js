ce05638808197bada06091f47ae3c1686a7a9d7c
iostate <qmtruong92@gmail.com>
2022-07-06 15:26:16 -0700

    Only admin can access the users

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,6 @@ router.use(protect);
 // Must be an admin to use ANY of these routes to perform CRUD on Users
 router.use(authorize('admin'));
 
-router.route('/').get(advancedResults(User), authorize('admin'), getUsers).post(createUser);
-router.route('/:id').get(authorize('admin'), getUser).put(updateUser).delete(deleteUser);
+router.route('/').get(advancedResults(User), getUsers).post(createUser);
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
 module.exports = router;
