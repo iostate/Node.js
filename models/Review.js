239fc402d973e9a17d84f9d1dbb04ac00ea3ba7a
iostate <qmtruong92@gmail.com>
2022-07-06 15:24:31 -0700

    + get avg. rating to ReviewSchema

--- models/Review.js
+++ models/Review.js
@@ -33,4 +33,36 @@ const ReviewSchema = new mongoose.Schema({
   },
 });
 
+ReviewSchema.static.getAverageRating = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: {
+          $avg: '$rating',
+        },
+      },
+    },
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+ReviewSchema.post('save', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Prevent user from submitting more than one review per bootcamp
+// @todo Prevent Duplicate Entry not working
+ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
+
 module.exports = mongoose.model('Review', ReviewSchema);
