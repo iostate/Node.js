8c347f1f16fadf39d1ad906b518e424c8c61d9ee
iostate <qmtruong92@gmail.com>
2022-07-06 16:51:40 -0700

    a

--- seeder.js
+++ seeder.js
@@ -37,7 +37,7 @@ const importData = async () => {
     console.log('Data Imported'.green.inverse);
     process.exit();
   } catch (err) {
-    console.log(err.red.inverse);
+    console.log(err);
   }
 };
 
