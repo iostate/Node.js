35278e7ed04d27ad024f234ea660a02fa58d6264
iostate <qmtruong92@gmail.com>
2022-07-21 11:38:09 -0700

    Add markdown

--- server.js
+++ server.js
@@ -24,6 +24,35 @@ const reviews = require('./routes/reviews');
 
 const app = express();
 
+var escapeHtml = require('escape-html');
+var fs = require('fs');
+var marked = require('marked');
+
+// register .md as an engine in express view system
+
+app.engine('md', function (path, options, fn) {
+  fs.readFile(path, 'utf8', function (err, str) {
+    if (err) return fn(err);
+    var html = marked.parse(str).replace(/\{([^}]+)\}/g, function (_, name) {
+      return escapeHtml(options[name] || '');
+    });
+    fn(null, html);
+  });
+});
+
+app.set('views', path.join(__dirname, 'views'));
+
+// make it the default so we dont need .md
+app.set('view engine', 'md');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Markdown Example' });
+});
+
+app.get('/fail', function (req, res) {
+  res.render('missing', { title: 'Markdown Example' });
+});
+
 app.use(helmet()); // Secure HTTP Headers
 app.use(cors()); // Cross origin requests
 app.use(express.json()); // Body parser
